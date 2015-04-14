2cb30d2d1e4037a93255c13e28c3ce6a71b0d44e
Evaldas Sudvajus <Evaldas.Sudvajus@visma.com>
2015-04-14 08:23:34 +0300

    added button method generation

---
+++ SPOC_Web/SpocWeb/Generate/Elements/ButtonElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace SpocWeb.Generate.Elements
+{
+    public class ButtonElement : BaseElement
+    {
+        public override string GenerateSetMethod(string name)
+        {
+            return "";
+        }
+
+        public override string GenerateClickMethod(string name)
+        {
+            var method = new StringBuilder();
+            method.AppendLine()
+                .Append(Utils.AddTextWithSpaces(string.Format("public XXX Click{0}()", Utils.UpperCaseFirstLetter(name))))
+                .AppendLine().Append(Utils.AddTextWithSpaces("{")).AppendLine();
+            Utils.IncreaseSpaceCounter();
+            method.Append(Utils.AddTextWithSpaces(string.Format("{0}.click();", name))).AppendLine();
+            method.Append(Utils.AddTextWithSpaces("return this;")).AppendLine();
+            Utils.DecreaseSpaceCounter();
+            method.Append(Utils.AddTextWithSpaces("}")).AppendLine();
+
+            return method.ToString();
+        }
+    }
+}
